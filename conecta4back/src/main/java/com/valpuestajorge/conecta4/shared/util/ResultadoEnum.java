b7aed9acf2ea22ac4a523bf998859620bc1c8718
JValpuesta <61006206+JValpuesta@users.noreply.github.com>
2024-11-04 22:01:00 +0100

    Delete conecta4back directory

--- conecta4back/src/main/java/com/valpuestajorge/conecta4/shared/util/ResultadoEnum.java
+++
@@ -1,9 +0,0 @@
-package com.valpuestajorge.conecta4.shared.util;
-
-public enum ResultadoEnum {
-    PLAYER_1("1"),
-    PLAYER_2("2"),
-    DRAW("DRAW");
-
-    ResultadoEnum(String resultado){}
-}
